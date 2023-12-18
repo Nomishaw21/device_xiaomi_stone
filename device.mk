98c8a5602acb6faf037e7db43c42f761aa69798e
TheStrechh <thestrechh@gmail.com>
2023-12-18 07:10:23 +0530

    stone: Add missing packages for camera
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>

--- device.mk
+++ device.mk
@@ -122,7 +122,10 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    libcamera2ndk_vendor \
+    libion.vendor \
+    libgui_vendor
 
 # Camera GrapheneCamera
 PRODUCT_PACKAGES += \
@@ -279,7 +282,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
+    libhidltransport \
     libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
