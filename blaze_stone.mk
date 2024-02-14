06c9a5cd5e11941c5470d765398dff9bced36bcc
Nomishaw21 <nomishaw9@gmail.com>
2024-02-14 17:50:33 +0000

    stone: bring up blaze
    Signed-off-by: Nomishaw21 <nomishaw9@hmail.com>
    Signed-off-by: Nomishaw21 <nomishaw9@gmail.com>

--- blaze_stone.mk
+++ blaze_stone.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common banana stuff.
-$(call inherit-product, vendor/banana/config/common.mk)
+# Inherit some common blaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
@@ -21,9 +21,10 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
 TARGET_DEBLOAT := true
 
-#Banana Flags
+#Blaze Flags
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := Nomi..
 WITH_GAPPS := true
-BANANA_MAINTAINER := Thereache
 
 # Charging Animation
 TARGET_USE_PIXEL_CHARGER := true
@@ -32,7 +33,7 @@ TARGET_USE_PIXEL_CHARGER := true
 PREBUILT_KERNEL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := banana_stone
+PRODUCT_NAME := blaze_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
