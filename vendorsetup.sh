75563344e4c4f4efbeff777a00a22f8437d381e5
Nomishaw21 <Nomishaw9@gmail.com>
2024-05-31 16:19:02 +0530

    stone: updated vendorsetup.sh
    Signed-off-by: Nomishaw21 <Nomishaw9gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,7 @@
-git clone --depth=1 https://github.com/Thereache/vendor_xiaomi_stone_unified.git -b 14 vendor/xiaomi
+git clone https://github.com/Nomishaw21/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375 --depth=1
 
-git clone --depth=1 https://github.com/Thereache/device_xiaomi_stone-kernel.git -b 14 device/xiaomi/stone-kernel
+git clone https://github.com/Nomishaw21/vendor_xiaomi_stone_unified -b 14 vendor/xiaomi/stone
 
-git clone --depth=1 https://github.com/Thereache/Stone_hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/Nomishaw21/device_xiaomi_stone-kernel -b 14 device/xiaomi/stone-kernel
+
+gti clone https://github.com/Nomishaw21/Vendor-micam vendor/MiuiCameraLeica
