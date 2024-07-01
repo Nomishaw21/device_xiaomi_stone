1b325e0dfdcbbe45209c339ec1fab34f6b261daf
kibria5 <106745675+kibria5@users.noreply.github.com>
2024-07-01 08:33:20 +0530

    stone: Add speed preopt for Trebuchet/Settings/SystemUI

--- device.mk
+++ device.mk
@@ -153,6 +153,12 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 USE_DEX2OAT_DEBUG := false
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    TrebuchetQuickStep \
+    Settings \
+    SystemUI
+
 # Disable SF configstore
 PRODUCT_PACKAGES += \
     disable_configstore
