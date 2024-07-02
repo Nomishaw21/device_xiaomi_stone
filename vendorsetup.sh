b33ccff03a3b8adb7119e8aae2201028754d6adc
Nomishaw21 <nomishaw9@gmail.com>
2024-07-02 11:48:27 +0530

    test

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,6 @@ git clone https://github.com/Nomishaw21/kernel_xiaomi_sm6375 kernel/xiaomi/sm637
 
 git clone https://github.com/Nomishaw21/vendor_xiaomi_stone_unified -b 14 vendor/xiaomi/stone
 
-git clone https://github.com/Nomishaw21/device_xiaomi_stone-kernel -b 14 device/xiaomi/stone-kernel
+git clone --depth=1  https://github.com/Nomishaw21/device_xiaomi_stone-kernel -b 14 device/xiaomi/stone-kernel
 
 git clone https://github.com/Nomishaw21/Vendor-micam vendor/MiuiCameraLeica
